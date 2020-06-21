bc19dba9bc881a74399c49ae6f230f2f55ea6b09
Anderson <anderson@prodwin.com.br>
2020-06-21 14:32:47 -0300

    Estudos Iniciais sobre Angular
    Estudos Iniciais sobre Angular

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // os Componentes
+  ],
+  imports: [
+    CommonModule
+    // os Módulos
+  ],
+  providers: [
+    // Seus Serviços
+  ]
+})
+export class FuncionalidadeModule { }
---
+++ MinhaAppAngular/src/app/pasta/meu-componente/meu-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-componente',
+  templateUrl: './meu-componente.component.html'
+})
+export class MeuComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
